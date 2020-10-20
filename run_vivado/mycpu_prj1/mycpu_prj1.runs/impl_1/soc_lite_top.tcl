f365502b045d3e09304d50970c3ee5423fc1fff0
ceba <ceba_robot@outlook.com>
2020-10-20 15:19:53 +0800

    finish(lab6): finish Synthesis & Implementation

--- run_vivado/mycpu_prj1/mycpu_prj1.runs/impl_1/soc_lite_top.tcl
+++ run_vivado/mycpu_prj1/mycpu_prj1.runs/impl_1/soc_lite_top.tcl
@@ -60,15 +60,106 @@ proc step_failed { step } {
   close $ch
 }
 
-set_msg_config -id {Common 17-41} -limit 10000000
+
+start_step init_design
+set ACTIVE_STEP init_design
+set rc [catch {
+  create_msg_db init_design.pb
+  set_param chipscope.maxJobs 2
+  create_project -in_memory -part xc7a200tfbg676-2
+  set_property design_mode GateLvl [current_fileset]
+  set_param project.singleFileAddWarning.threshold 0
+  set_property webtalk.parent_dir C:/Users/ceba_/Documents/working/verilog/CPU_CDE/mycpu_verify/run_vivado/mycpu_prj1/mycpu_prj1.cache/wt [current_project]
+  set_property parent.project_path C:/Users/ceba_/Documents/working/verilog/CPU_CDE/mycpu_verify/run_vivado/mycpu_prj1/mycpu_prj1.xpr [current_project]
+  set_property ip_output_repo C:/Users/ceba_/Documents/working/verilog/CPU_CDE/mycpu_verify/run_vivado/mycpu_prj1/mycpu_prj1.cache/ip [current_project]
+  set_property ip_cache_permissions {read write} [current_project]
+  set_property XPM_LIBRARIES {XPM_CDC XPM_MEMORY} [current_project]
+  add_files -quiet C:/Users/ceba_/Documents/working/verilog/CPU_CDE/mycpu_verify/run_vivado/mycpu_prj1/mycpu_prj1.runs/synth_1/soc_lite_top.dcp
+  read_ip -quiet C:/Users/ceba_/Documents/working/verilog/CPU_CDE/mycpu_verify/run_vivado/mycpu_prj1/mycpu_prj1.srcs/sources_1/ip/signed_divider/signed_divider.xci
+  read_ip -quiet C:/Users/ceba_/Documents/working/verilog/CPU_CDE/mycpu_verify/run_vivado/mycpu_prj1/mycpu_prj1.srcs/sources_1/ip/unsigned_divider/unsigned_divider.xci
+  read_ip -quiet C:/Users/ceba_/Documents/working/verilog/CPU_CDE/mycpu_verify/rtl/xilinx_ip/clk_pll/clk_pll.xci
+  read_ip -quiet C:/Users/ceba_/Documents/working/verilog/CPU_CDE/mycpu_verify/rtl/xilinx_ip/data_ram/data_ram.xci
+  read_ip -quiet C:/Users/ceba_/Documents/working/verilog/CPU_CDE/mycpu_verify/rtl/xilinx_ip/inst_ram/inst_ram.xci
+  read_xdc C:/Users/ceba_/Documents/working/verilog/CPU_CDE/mycpu_verify/run_vivado/soc_lite.xdc
+  link_design -top soc_lite_top -part xc7a200tfbg676-2
+  close_msg_db -file init_design.pb
+} RESULT]
+if {$rc} {
+  step_failed init_design
+  return -code error $RESULT
+} else {
+  end_step init_design
+  unset ACTIVE_STEP 
+}
+
+start_step opt_design
+set ACTIVE_STEP opt_design
+set rc [catch {
+  create_msg_db opt_design.pb
+  opt_design 
+  write_checkpoint -force soc_lite_top_opt.dcp
+  create_report "impl_1_opt_report_drc_0" "report_drc -file soc_lite_top_drc_opted.rpt -pb soc_lite_top_drc_opted.pb -rpx soc_lite_top_drc_opted.rpx"
+  close_msg_db -file opt_design.pb
+} RESULT]
+if {$rc} {
+  step_failed opt_design
+  return -code error $RESULT
+} else {
+  end_step opt_design
+  unset ACTIVE_STEP 
+}
+
+start_step place_design
+set ACTIVE_STEP place_design
+set rc [catch {
+  create_msg_db place_design.pb
+  if { [llength [get_debug_cores -quiet] ] > 0 }  { 
+    implement_debug_core 
+  } 
+  place_design 
+  write_checkpoint -force soc_lite_top_placed.dcp
+  create_report "impl_1_place_report_io_0" "report_io -file soc_lite_top_io_placed.rpt"
+  create_report "impl_1_place_report_utilization_0" "report_utilization -file soc_lite_top_utilization_placed.rpt -pb soc_lite_top_utilization_placed.pb"
+  create_report "impl_1_place_report_control_sets_0" "report_control_sets -verbose -file soc_lite_top_control_sets_placed.rpt"
+  close_msg_db -file place_design.pb
+} RESULT]
+if {$rc} {
+  step_failed place_design
+  return -code error $RESULT
+} else {
+  end_step place_design
+  unset ACTIVE_STEP 
+}
+
+start_step route_design
+set ACTIVE_STEP route_design
+set rc [catch {
+  create_msg_db route_design.pb
+  route_design 
+  write_checkpoint -force soc_lite_top_routed.dcp
+  create_report "impl_1_route_report_drc_0" "report_drc -file soc_lite_top_drc_routed.rpt -pb soc_lite_top_drc_routed.pb -rpx soc_lite_top_drc_routed.rpx"
+  create_report "impl_1_route_report_methodology_0" "report_methodology -file soc_lite_top_methodology_drc_routed.rpt -pb soc_lite_top_methodology_drc_routed.pb -rpx soc_lite_top_methodology_drc_routed.rpx"
+  create_report "impl_1_route_report_power_0" "report_power -file soc_lite_top_power_routed.rpt -pb soc_lite_top_power_summary_routed.pb -rpx soc_lite_top_power_routed.rpx"
+  create_report "impl_1_route_report_route_status_0" "report_route_status -file soc_lite_top_route_status.rpt -pb soc_lite_top_route_status.pb"
+  create_report "impl_1_route_report_timing_summary_0" "report_timing_summary -max_paths 10 -file soc_lite_top_timing_summary_routed.rpt -pb soc_lite_top_timing_summary_routed.pb -rpx soc_lite_top_timing_summary_routed.rpx -warn_on_violation "
+  create_report "impl_1_route_report_incremental_reuse_0" "report_incremental_reuse -file soc_lite_top_incremental_reuse_routed.rpt"
+  create_report "impl_1_route_report_clock_utilization_0" "report_clock_utilization -file soc_lite_top_clock_utilization_routed.rpt"
+  create_report "impl_1_route_report_bus_skew_0" "report_bus_skew -warn_on_violation -file soc_lite_top_bus_skew_routed.rpt -pb soc_lite_top_bus_skew_routed.pb -rpx soc_lite_top_bus_skew_routed.rpx"
+  close_msg_db -file route_design.pb
+} RESULT]
+if {$rc} {
+  write_checkpoint -force soc_lite_top_routed_error.dcp
+  step_failed route_design
+  return -code error $RESULT
+} else {
+  end_step route_design
+  unset ACTIVE_STEP 
+}
 
 start_step write_bitstream
 set ACTIVE_STEP write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
-  set_param chipscope.maxJobs 2
-  open_checkpoint soc_lite_top_routed.dcp
-  set_property webtalk.parent_dir C:/Users/ceba_/Documents/working/verilog/CPU_CDE/mycpu_verify/run_vivado/mycpu_prj1/mycpu_prj1.cache/wt [current_project]
   set_property XPM_LIBRARIES {XPM_CDC XPM_MEMORY} [current_project]
   catch { write_mem_info -force soc_lite_top.mmi }
   write_bitstream -force soc_lite_top.bit 
--- run_vivado/mycpu_prj1/mycpu_prj1.runs/synth_1/soc_lite_top.tcl
+++ run_vivado/mycpu_prj1/mycpu_prj1.runs/synth_1/soc_lite_top.tcl
@@ -17,7 +17,6 @@ proc create_report { reportName command } {
     send_msg_id runtcl-5 warning "$msg"
   }
 }
-set_msg_config -id {Common 17-41} -limit 10000000
 create_project -in_memory -part xc7a200tfbg676-2
 
 set_param project.singleFileAddWarning.threshold 0
@@ -33,6 +32,7 @@ set_property ip_output_repo c:/Users/ceba_/Documents/working/verilog/CPU_CDE/myc
 set_property ip_cache_permissions {read write} [current_project]
 add_files C:/Users/ceba_/Documents/working/verilog/CPU_CDE/soft/func_lab3/obj/inst_ram.coe
 add_files C:/Users/ceba_/Documents/working/verilog/CPU_CDE/soft/func_lab4/obj/inst_ram.coe
+add_files C:/Users/ceba_/Documents/working/verilog/CPU_CDE/soft/func_lab6/obj/inst_ram.coe
 read_verilog C:/Users/ceba_/Documents/working/verilog/CPU_CDE/mycpu_verify/rtl/myCPU/mycpu.h
 set_property file_type "Verilog Header" [get_files C:/Users/ceba_/Documents/working/verilog/CPU_CDE/mycpu_verify/rtl/myCPU/mycpu.h]
 read_verilog -library xil_defaultlib {
@@ -49,6 +49,12 @@ read_verilog -library xil_defaultlib {
   C:/Users/ceba_/Documents/working/verilog/CPU_CDE/mycpu_verify/rtl/myCPU/tools.v
   C:/Users/ceba_/Documents/working/verilog/CPU_CDE/mycpu_verify/rtl/soc_lite_top.v
 }
+read_ip -quiet C:/Users/ceba_/Documents/working/verilog/CPU_CDE/mycpu_verify/run_vivado/mycpu_prj1/mycpu_prj1.srcs/sources_1/ip/signed_divider/signed_divider.xci
+set_property used_in_implementation false [get_files -all c:/Users/ceba_/Documents/working/verilog/CPU_CDE/mycpu_verify/run_vivado/mycpu_prj1/mycpu_prj1.srcs/sources_1/ip/signed_divider/signed_divider_ooc.xdc]
+
+read_ip -quiet C:/Users/ceba_/Documents/working/verilog/CPU_CDE/mycpu_verify/run_vivado/mycpu_prj1/mycpu_prj1.srcs/sources_1/ip/unsigned_divider/unsigned_divider.xci
+set_property used_in_implementation false [get_files -all c:/Users/ceba_/Documents/working/verilog/CPU_CDE/mycpu_verify/run_vivado/mycpu_prj1/mycpu_prj1.srcs/sources_1/ip/unsigned_divider/unsigned_divider_ooc.xdc]
+
 read_ip -quiet C:/Users/ceba_/Documents/working/verilog/CPU_CDE/mycpu_verify/rtl/xilinx_ip/clk_pll/clk_pll.xci
 set_property used_in_implementation false [get_files -all c:/Users/ceba_/Documents/working/verilog/CPU_CDE/mycpu_verify/rtl/xilinx_ip/clk_pll/clk_pll_board.xdc]
 set_property used_in_implementation false [get_files -all c:/Users/ceba_/Documents/working/verilog/CPU_CDE/mycpu_verify/rtl/xilinx_ip/clk_pll/clk_pll.xdc]
@@ -71,6 +77,8 @@ foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
 read_xdc C:/Users/ceba_/Documents/working/verilog/CPU_CDE/mycpu_verify/run_vivado/soc_lite.xdc
 set_property used_in_implementation false [get_files C:/Users/ceba_/Documents/working/verilog/CPU_CDE/mycpu_verify/run_vivado/soc_lite.xdc]
 
+read_xdc dont_touch.xdc
+set_property used_in_implementation false [get_files dont_touch.xdc]
 set_param ips.enableIPCacheLiteLoad 1
 close [open __synthesis_is_running__ w]
 
